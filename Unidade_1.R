0a36e0f18a3de3711af2ba1856b4e5383cd7d431
ademir-maneira <ademir.maneira@gmail.com>
2020-12-19 15:30:54 -0300

    Primeiro arquivo

---
+++ Unidade_1.R
@@ -0,0 +1,7 @@
+renan@atualcargo.com.br
+paulorenatoramos@hotmail.com
+multvendas.nfe@gmail.com
+kelvin.santos@multvendasdistribuicao.com.br
+diego@bmgfi.com
+financeiroundimemg@gmail.com
+tesouraria@uppe.com.br
